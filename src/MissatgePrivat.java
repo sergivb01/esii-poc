1f6c7eba62164d2397fcbfb1027403deba60a9fb
Sergi Vos <sergivb01@gmail.com>
2022-01-07 00:48:29 +0100

    inici MissatgePrivat

--- src/MissatgePrivat.java
+++ src/MissatgePrivat.java
@@ -1,3 +1,32 @@
+import java.util.List;
+
 public class MissatgePrivat {
+
+  private final Usuari remitent;
+  private final Usuari destinatari;
+  private final List<Paraula> paraules;
+
+
+  public MissatgePrivat(Usuari remitent, Usuari destinatari, List<Paraula> paraules) {
+    this.remitent = remitent;
+    this.destinatari = destinatari;
+    this.paraules = paraules;
+  }
+
+  public Usuari remitent() {
+    return this.remitent;
+  }
+
+  public Usuari destinatari() {
+    return this.destinatari;
+  }
+
+  public String contingut() {
+    StringBuilder sb = new StringBuilder();
+    for (Paraula paraula : paraules) {
+      sb.append(paraula.contingut());
+    }
+    return sb.toString();
+  }
 
 }
