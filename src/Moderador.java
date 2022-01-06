b1adc3363d345bddaf014a475156a27eae7015f2
Sergi Vos <sergivb01@gmail.com>
2022-01-07 00:44:11 +0100

    Moderador

---
+++ src/Moderador.java
@@ -0,0 +1,7 @@
+// el moderador pot crear/modificar/eliminar publicacions d'altres usuaris
+public class Moderador extends Usuari {
+
+  public Moderador(String nom, String cognom) {
+    super(nom, cognom);
+  }
+}
