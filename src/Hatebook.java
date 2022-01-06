40544c1b0c2cafaa3948146415662646db86caf2
Sergi Vos <sergivb01@gmail.com>
2022-01-07 00:03:04 +0100

    relacions

--- src/Hatebook.java
+++ src/Hatebook.java
@@ -1,10 +1,27 @@
 public class Hatebook {
 
   public static void main(String[] args) {
-    System.out.println(new ParaulaMarejada("hello"));
-    final ParaulaBipolar paraula = new ParaulaBipolar("test", 50);
-    for (int i = 0; i < 10; i++) {
-      System.out.println(i + ": " + paraula);
+    {
+      // comprovar paraula marejada i bipolar
+      System.out.println(new ParaulaMarejada("hello"));
+      final ParaulaBipolar paraula = new ParaulaBipolar("test", 50);
+      for (int i = 0; i < 10; i++) {
+        System.out.println(i + ": " + paraula);
+      }
+    }
+
+    {
+      // comprovar relacions
+      final Usuari tom = new Usuari("tom", "simons");
+      final Usuari test = new Usuari("test", "ing");
+
+      System.out.println(tom.tipusRelacio(test));
+      tom.canviarRelacioUsuari(test, TipusRelacio.AMIC);
+      System.out.println(tom.tipusRelacio(test));
+      tom.canviarRelacioUsuari(test, TipusRelacio.SALUDAT);
+      System.out.println(tom.tipusRelacio(test));
+      tom.canviarRelacioUsuari(test, TipusRelacio.RES);
+      System.out.println(tom.tipusRelacio(test));
     }
   }
 
--- src/Usuari.java
+++ src/Usuari.java
@@ -1,11 +1,37 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Usuari {
 
   private final String nom;
   private final String cognom;
+  private final Map<Usuari, TipusRelacio> relacions;
 
   public Usuari(String nom, String cognom) {
     this.nom = nom;
     this.cognom = cognom;
+    this.relacions = new LinkedHashMap<>();
   }
 
+  public void canviarRelacioUsuari(final Usuari usuari, final TipusRelacio tipus) {
+    if (tipus == TipusRelacio.RES) {
+      // si ja no tenen cap relació, el treiem del mapa per estalviar espai amb buits...
+      relacions.remove(usuari);
+      return;
+    }
+
+    relacions.put(usuari, tipus);
+  }
+
+  public TipusRelacio tipusRelacio(final Usuari usuari) {
+    return relacions.getOrDefault(usuari, TipusRelacio.RES);
+  }
+
+  public String nom() {
+    return this.nom;
+  }
+
+  public String cognom() {
+    return this.cognom;
+  }
 }
