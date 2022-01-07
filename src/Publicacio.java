02439eaa99a871fe2f60002164fc6d2c7d2b8ba9
Xavier Terés <xavier.teres@somenergia.coop>
2022-01-07 23:14:06 +0100

    Implementada una mica classe Publicacio

--- src/Publicacio.java
+++ src/Publicacio.java
@@ -1,3 +1,30 @@
+import java.util.List;
+
 public class Publicacio {
+    private final Usuari propietari;
+    private final TipusRelacio visibilitat;
+    private List<Paraula> paraules;
+
+    public Publicacio(Usuari propietari, TipusRelacio visibilitat, List<Paraula> paraules) {
+        this.propietari = propietari;
+        this.visibilitat = visibilitat;
+        this.paraules = paraules;
+    }
+
+    public Usuari propietari() {
+        return this.propietari;
+    }
+
+    public String contingut() {
+        StringBuilder sb = new StringBuilder();
+        for (Paraula paraula : paraules) {
+            sb.append(paraula.contingut());
+        }
+        return sb.toString();
+    }
+
+    public void nou_contingut(List<Paraula> paraules) {
+        this.paraules = paraules;
+    }
 
 }
